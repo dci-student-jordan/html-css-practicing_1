da6b27b6ad932ee573c8c586308b2b733709c133
Reinhard <reinhard.jordan@dci-student.org>
2023-07-03 15:42:35 +0200

    embed pages with iframe

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-//show/hide login
+//show or hide login
 
 const loginShower = document.querySelector('#loginShower')
 const loginForm = document.getElementById('loginForm')
@@ -25,12 +25,26 @@ function isValidMessage () {
     var em = document.querySelector('#email').value
     var isEmail = /^\S+@\S+\.\S+$/.test(em)
     submitter = document.querySelector('#submitMess')
-    if (isEmail & (document.querySelector('#message').value != "")) {
+    if (isEmail &
+        (document.querySelector('#message').value != "") &
+        (document.querySelector('#name').value != "")) {
             submitter.disabled = false;
     }
     else submitter.disabled = true;
 }
 
 function sendMess () {
-    window.location.href = "mailto:"+document.querySelector('#email').value+"?subject=nicely submitted Mail&body="+document.querySelector('#message').value
+    window.location.href = "mailto:"+document.querySelector('#email').value+"?subject=nicely submitted Mail by "+document.querySelector('#name').value+"&body="+document.querySelector('#message').value
+}
+
+// unblock alert in iframed content
+
+function unblockAlert () {
+    var unblockButton = document.querySelector('#allowmodal')
+    var atr = (unblockButton.textContent =='unblock alert' ) ? "allow-scripts allow-modals" : "allow-scripts"
+    var buttonString = (unblockButton.textContent =='unblock alert' ) ? 'block again' : 'unblock alert'
+        document.querySelector('#jspage2').setAttribute("sandbox", atr)
+        unblockButton.textContent = buttonString
+        //content needs to b reloaded, otherwise it won't work
+        document.querySelector('#jspage2').setAttribute("src", "./downloads/page-js-2.html")
 }
